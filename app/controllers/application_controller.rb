24cb2318b10212f8b46b064239a1d98780ef26a7
Johnnae Tiffany <jrtiff0221@gmail.com>
2020-03-13 03:11:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+def logged_in?
+  !!current_user
+end
+
+def current_user
+
+  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+end
+
+def require_login
+  redirect_to root_url unless current_user
+end
 end
